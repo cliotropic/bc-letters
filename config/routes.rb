2fbaeb15c9cf5badea5461a208c0257d35606306
Shane Landrum <srl@cliotropic.org>
2010-05-13 13:25:17 -0400

    Adjusted routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :document_files
+
+  map.resources :files
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
